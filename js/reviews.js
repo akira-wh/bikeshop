2231abad95017cdc401ace730bec9e5cffdc78cd
Akira <154.4k1r4@gmail.com>
2018-01-18 04:47:03 +0600

    1.02 Реализована отрисовка комментариев. Улучшена разметка и стилизация.

---
+++ js/reviews.js
@@ -0,0 +1,66 @@
+'use strict';
+
+/*
+***********************************************************************************
+***********************************************************************************
+***
+***                           МЕХАНИКА ОТРИСОВКИ ОТЗЫВОВ
+***
+***********************************************************************************
+***********************************************************************************
+*/
+
+(function () {
+
+  // Навешивание на кнопку "Показать еще" хендлера нажатия
+  window.constants.MORE_REVIEWS_BUTTON.addEventListener('click', onMoreReviewsButtonClick);
+
+  /**
+   * При клике по кнопке "Показать еще":
+   * отрисовка следующих комментариев.
+   *
+   * @function onMoreReviewsButtonClick
+   */
+  function onMoreReviewsButtonClick() {
+    renderReviews();
+  }
+
+  /*
+  ***********************************************************************************
+  ***********************************************************************************
+  ***
+  ***                             ВСПОМОГАТЕЛЬНЫЕ ФУНКЦИИ
+  ***
+  ***********************************************************************************
+  ***********************************************************************************
+  */
+
+  /**
+   * Отрисовка следующих 5-и комментариев из базы.
+   * Если комментариев не набирается нужное количество — отрисовывается то, что есть.
+   *
+   * @function renderReviews
+   */
+  function renderReviews() {
+    var reviewsStartIndex = Number(window.constants.MORE_REVIEWS_BUTTON.dataset.count);
+    var reviewsFragment = document.createDocumentFragment();
+
+    for (var i = reviewsStartIndex; i < reviewsStartIndex + 5; i++) {
+      var review = window.constants.REVIEW_TEMPLATE.cloneNode(true);
+      var reviewText = review.querySelector('.reviews__quote-text');
+      var reviewAuthor = review.querySelector('.reviews__quote-author');
+
+      if (window.data[i]) {
+        reviewAuthor.textContent = window.data[i].author;
+        reviewText.textContent = window.data[i].text;
+        reviewsFragment.appendChild(review);
+      } else {
+        window.constants.MORE_REVIEWS_BUTTON.textContent = 'Конец списка';
+        break;
+      }
+    }
+
+    window.constants.REVIEWS_CONTAINER.insertBefore(reviewsFragment, window.constants.MORE_REVIEWS_BUTTON);
+    window.constants.MORE_REVIEWS_BUTTON.dataset.count = i;
+  }
+})();
